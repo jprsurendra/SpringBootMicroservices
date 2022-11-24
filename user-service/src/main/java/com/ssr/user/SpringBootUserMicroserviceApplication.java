d65aab2a53f6769bfe4fee8b99b97082d7fbce79
Surendra <jpr_surendra@yahoo.co.in>
2022-11-24 14:09:02 +0530

    new Microservices --  user-service

---
+++ user-service/src/main/java/com/ssr/user/SpringBootUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ssr.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserMicroserviceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/ssr/user/SpringBootUserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssr.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
